5a82fe7da977cc4c7c1d7360858a8927f34e6407
Marcio <marciomarim@gmail.com>
2020-08-27 15:59:38 -0300

    v0.0.9

--- js/init.js
+++ js/init.js
@@ -2,10 +2,15 @@ const os = require('os');
 
 var fs = require('fs');
 
+const seven = require('node-7z');
+//const p7zip = require('p7zip');
+
 const Store = require('electron-store'); 
 const store = new Store();
 
 const homedir = os.homedir();
+
+const {ipcRenderer} = require("electron");
 
 const shell = require('electron').shell;
 
@@ -31,7 +36,32 @@ if (os.platform() == 'linux' || os.platform() == 'darwin'){
 		var enginepath = "/var/etc/spring";
 	}
 	
+	if (!fs.existsSync(springdir)){
+	    fs.mkdirSync(springdir);
+	}
+	
+	if (!fs.existsSync(mapsdir)){
+	    fs.mkdirSync(mapsdir);
+	}
+	
+	if (!fs.existsSync(modsdir)){
+	    fs.mkdirSync(modsdir);
+	}
+	
+	if (!fs.existsSync(replaysdir)){
+	    fs.mkdirSync(replaysdir);
+	}
+	
+	if (!fs.existsSync(chatlogsdir)){
+	    fs.mkdirSync(chatlogsdir);
+	}
+	
 }else if(os.platform() == 'win32'){
+	
+	var mygamesdir = homedir + '\\Documents\\My Games\\';
+	if (!fs.existsSync(mygamesdir)){
+	    fs.mkdirSync(mygamesdir);
+	}
 	
 	var springdir = homedir + '\\Documents\\My Games\\Spring\\';
 	var mapsdir = homedir + '\\Documents\\My Games\\Spring\\maps\\';
@@ -40,12 +70,93 @@ if (os.platform() == 'linux' || os.platform() == 'darwin'){
 	var chatlogsdir = homedir + '\\Documents\\My Games\\Spring\\chatlogs\\';
 	var infologfile = homedir + '\\Documents\\My Games\\Spring\\infolog.log';
 	var scriptfile = homedir + '\\Documents\\My Games\\Spring\\e-script.txt';
+	var enginedir = homedir + '\\Documents\\My Games\\Spring\\engine\\';
 	var enginepath = homedir + '\\Documents\\My Games\\Spring\\engine\\103.0\\spring.exe';
+	
+	if (!fs.existsSync(springdir)){
+	    fs.mkdirSync(springdir);
+	}
+	
+	if (!fs.existsSync(mapsdir)){
+	    fs.mkdirSync(mapsdir);
+	}
+	
+	if (!fs.existsSync(modsdir)){
+	    fs.mkdirSync(modsdir);
+	}
+	
+	if (!fs.existsSync(replaysdir)){
+	    fs.mkdirSync(replaysdir);
+	}
+	
+	if (!fs.existsSync(chatlogsdir)){
+		console.log('Creating chatlog folder');
+	    fs.mkdirSync(chatlogsdir);
+	}
+	
+	if (!fs.existsSync(enginedir)){
+		console.log('Creating engine folder');
+		fs.mkdirSync(enginedir);
+	}
+	
+	if (!fs.existsSync(enginepath)) {
+		
+		var engineurl = 'https://springrts.com/dl/buildbot/default/master/103.0/win64/spring_103.0_win64_portable.7z';
+	    console.log('Download spring');
+	    
+	    $.ajax({ 
+            url: engineurl, 
+            type: 'HEAD', 
+            error: function()  
+            { 
+                console.log('Engine not found!');                                
+            }, 
+            success: function()  
+            {                 
+                downloadengine(engineurl);
+            } 
+        }); 
+	}
+	// not portable
+/*
+	if (!fs.existsSync(springdir)){
+		springdir = 'C:\\Program Files (x86)\\Spring\\';
+		var mapsdir = 'C:\\Program Files (x86)\\Spring\\maps\\';
+		var modsdir = 'C:\\Program Files (x86)\\Spring\\games\\';
+		var replaysdir = 'C:\\Program Files (x86)\\Spring\\demos\\';
+		var chatlogsdir = 'C:\\Program Files (x86)\\Spring\\chatlogs\\';
+		var infologfile = 'C:\\Program Files (x86)\\Spring\\infolog.log';
+		var scriptfile = 'C:\\Program Files (x86)\\Spring\\e-script.txt';
+		var enginepath = 'C:\\Program Files (x86)\\Spring\\spring.exe';
+	}	
+*/	
 }
 
 
-if (!fs.existsSync(chatlogsdir)){
-    fs.mkdirSync(chatlogsdir);
+
+function downloadengine(fileurl){
+    
+    ipcRenderer.send("download", {
+	    url: fileurl,
+	    properties: {directory: enginedir}
+	});			
+	
+	ipcRenderer.on("download progress", async (event, progress) => {		
+		//$('#battleroom .map-download').addClass('downloading');
+		var w = Math.round( progress.percent*100 ) + '%';
+		console.log('Downloading engine: ' + w + ' of 100%');
+		//$('#battleroom .map-download .download-title').text('Downloading map: ' + w + ' of 100%');
+		//$('#battleroom .map-download .progress').css('width', w);											
+	});
+	
+	ipcRenderer.on("download complete", (event, progress) => {
+		console.log('Engine download: completed!');
+		//p7zip.extract(enginedir + 'spring_103.0_win64_portable.7z', enginedir);
+		const myStream = seven.extractFull(enginedir + 'spring_103.0_win64_portable.7z', enginedir, { 
+			$progress: true
+		})
+	});
+	
 }
 
 var remotemodsurl = 'https://springfightclub.com/data/';
