9ebe9fd520715650870d9a198b238fa49d889feb
Marcio <marciomarim@gmail.com>
2021-02-01 15:24:59 +0000

    removing borderless on windows as it’s brokem

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function createWindow() {
 		backgroundColor: '#000000',
 		//icon: __dirname + '/assets/icons/icon.png',
 		titleBarStyle: 'hidden',
-		frame: false,
+		frame: true,
 		show: false,
 		webPreferences: {
 			nodeIntegration: true,
